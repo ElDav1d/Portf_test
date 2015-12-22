84263c3ed610c93be4628492403f12e9b1736750
ElDav1d <dvivoes@yahoo.es>
2015-12-22 13:54:20 +0100

    Restart for filterable portfolio (add files)

--- js/main.js
+++ js/main.js
@@ -43,6 +43,20 @@ $('select').dropdown({
 
 })
 
+$(document).ready(function(){
+  $('portfolio-list').filterable({
+    useHash: true,
+    animationSpeed: 1000,
+    show: { width: 'show', opacity: 'show' },
+    hide: { width: 'hide', opacity: 'hide' },
+    useTags: true,
+    tagSelector: '#portfolio-filter a',
+    selectedTagClass: 'current',
+    allTag: 'all'
+  });
+});
+
+
 
 
 var map = L.map('map').setView([39.464741, -0.375517],15);
