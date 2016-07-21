13f9c025a0a2a8e4170f97a6413b8b163542a289
ElDav1d <dvivoes@yahoo.es>
2016-07-21 14:07:15 +0200

    Added mixitup

---
+++ js/main.js
@@ -0,0 +1,116 @@
+
+
+$(document).foundation();
+
+
+
+$(document).ready(function(){
+ 
+ $('.chocolat-parent').Chocolat({
+ 	imageSize : "cover"
+ 	});
+ });
+
+
+
+
+
+
+$(function() {
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top -20
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+$('select').dropdown({
+
+  animate: true,
+  speed: 300,
+  autoClose: true,
+  autoToggle: true,
+  autoTitle: true,
+  autoResize: 300
+
+})
+
+
+
+var map = L.map('map').setView([39.464741, -0.375517],15);
+
+var Esri_WorldGrayCanvas = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 25
+}).addTo(map);
+
+var marker = L.marker([39.464741, -0.375517]).addTo(map);
+
+
+
+
+
+
+$(function () {
+  
+  var filterList = {
+  
+    init: function () {
+    
+      // MixItUp plugin
+      // http://mixitup.io
+      $('#portfoliolist').mixItUp({
+        selectors: {
+          target: '.portfolio',
+          filter: '.filter' 
+        },
+        load: {
+          filter: '.app'  
+        }     
+      });               
+    
+    }
+
+  };
+  
+  // Run the show!
+  filterList.init();
+  
+  
+}); 
+
+
+$(function () {
+    
+  var filterList = {
+  
+    init: function () {
+    
+      // MixItUp plugin
+      // http://mixitup.io
+      $('#portfoliolist').mixItUp({
+        selectors: {
+          target: '.portfolio',
+          filter: '.filter' 
+        },
+        load: {
+          filter: '.app' // show app tab on first load
+        }     
+      });               
+    
+    }
+
+  };
+  
+  // Run the show!
+  filterList.init();
+  
+}); 
